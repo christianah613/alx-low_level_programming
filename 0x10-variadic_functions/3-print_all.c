a4760571151813af88ce05aa9ba886df695c01e4
christianah613 <christianah613@gmail.com>
2022-10-13 02:05:04 -0700

    1-print_numbers.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,23 @@
+/*
+ *
+ *  * File: 3-print_all.c
+ *
+ *   * Auth: Tolulope Fakunle
+ *
+ *    */
+
+
+
+#include "variadic_functions.h"
+
+#include <stdio.h>
+
+#include <stdarg.h>
+
+
+
+void print_char(va_list arg);
+
+void print_int(va_list arg);
+
+void print_float(va_list arg);
